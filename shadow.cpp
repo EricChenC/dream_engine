461f332b1439bcf86334a3e251865f3ce8e0bae5
Eric <nameericchen@gmail.com>
2017-05-14 11:49:13 +0800

    add shadow advance version

--- shadow.cpp
+++ shadow.cpp
@@ -2,10 +2,11 @@
 
 #include <qmatrix4x4.h>
 
-#define DEPTH_TEXTURE_SIZE 512
+#define DEPTH_TEXTURE_SIZE 2048
 
 Shadow::Shadow()
 {
+	kAdvance_ = true;
 }
 
 
@@ -30,20 +31,12 @@ void Shadow::initializeGL()
 	depth_program_ = shader_->LoadShaders(depth_shaders);
 	depth_mvp_loc_ = gl_->glGetUniformLocation(depth_program_, "mvp");
 
-	de::ShaderInfo shadow_shaders[] =
-	{
-		{ GL_VERTEX_SHADER, "shaders/shadow/shadowmap.vs.glsl" },
-		{ GL_FRAGMENT_SHADER, "shaders/shadow/shadowmap.fs.glsl" },
-		{ GL_NONE }
-	};
-
-	shadow_program_ = shader_->LoadShaders(shadow_shaders);
-	shadow_mvp_loc_ = gl_->glGetUniformLocation(shadow_program_, "mvp");
-	shadow_bias_mvp_loc_ = gl_->glGetUniformLocation(shadow_program_, "bias_mvp");
-	shadow_map_tex_loc_ = gl_->glGetUniformLocation(shadow_program_, "shadowMap");
-
-	gl_->glUseProgram(shadow_program_);
-	gl_->glUniform1i(shadow_map_tex_loc_, 0);
+	if (kAdvance_) {
+		AdvanceVersionInit();
+	}
+	else {
+		SimpeVersionInit();
+	}
 	
 	gl_->glGenFramebuffers(1, &depth_fbo_);
 	gl_->glBindFramebuffer(GL_FRAMEBUFFER, depth_fbo_);
@@ -70,35 +63,14 @@ void Shadow::initializeGL()
 
 void Shadow::paintGL(const int& time)
 {
-	// qt matrix is a row major
-	// glm matrix is a column major
-	const QMatrix4x4 bias_matrix(
-		0.5f, 0.0f, 0.0f, 0.5f,
-		0.0f, 0.5f, 0.0f, 0.5f,
-		0.0f, 0.0f, 0.5f, 0.5f,
-		0.0f, 0.0f, 0.0f, 1.0f);
-
-	QMatrix4x4 mvp = player_camera_->get_mvp();
-
-	QMatrix4x4 light_proj;
-	light_proj.ortho(-10, 10, -10, 10, -10, 20);
-
-	QMatrix4x4 light_view;
-	// equal 0.0 result is error!  so i just write 0.001
-	light_view.lookAt(QVector3D(0.001f, 5.0f, 5.0f), QVector3D(0.0f, 0.0f, 0.0f), QVector3D(0.0f, 1.0f, 0.0f));
-
-	QMatrix4x4 light_model;
-	light_model.setToIdentity();
-
-	QMatrix4x4 light_mvp = light_proj * light_view;
-	QMatrix4x4 bias_light_mvp = bias_matrix * light_mvp;
+	InitData();
 
 	gl_->glEnable(GL_CULL_FACE);
 	gl_->glEnable(GL_DEPTH_TEST);
 	gl_->glDepthFunc(GL_LEQUAL);
 
 	gl_->glUseProgram(depth_program_);
-	gl_->glUniformMatrix4fv(depth_mvp_loc_, 1, GL_FALSE, light_mvp.constData());
+	gl_->glUniformMatrix4fv(depth_mvp_loc_, 1, GL_FALSE, &light_mvp_[0][0]);
 
 	gl_->glBindFramebuffer(GL_FRAMEBUFFER, depth_fbo_);
 	gl_->glViewport(0, 0, DEPTH_TEXTURE_SIZE, DEPTH_TEXTURE_SIZE);
@@ -117,11 +89,12 @@ void Shadow::paintGL(const int& time)
 
 	gl_->glViewport(0, 0, get_w(), get_h());
 
-	gl_->glUseProgram(shadow_program_);
-	gl_->glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-	gl_->glUniformMatrix4fv(shadow_mvp_loc_, 1, GL_FALSE, mvp.constData());
-	gl_->glUniformMatrix4fv(shadow_bias_mvp_loc_, 1, GL_FALSE, bias_light_mvp.constData());
+	if (kAdvance_) {
+		AdvanceVersionPaint();
+	}
+	else {
+		SimpeVersionPaint();
+	}
 
 	gl_->glActiveTexture(GL_TEXTURE0);
 	gl_->glBindTexture(GL_TEXTURE_2D, depth_texture_);
@@ -135,14 +108,143 @@ void Shadow::paintGL(const int& time)
 
 void Shadow::disableGL()
 {
-	gl_->glDisable(GL_CULL_FACE);
-	gl_->glDisable(GL_DEPTH_TEST);
+	if (kAdvance_) {
+		AdvanceVersionDisable();
+	}
+	else {
+		SimpeVersionDisable();
+	}
 }
 
 void Shadow::clearGL()
 {
+	if (kAdvance_) {
+		AdvanceVersionClear();
+	}
+	else {
+		SimpeVersionClear();
+	}
+}
+
+void Shadow::InitData()
+{
+	// qt matrix is a row major
+	// glm matrix is a column major
+	m_ = player_camera_->get_m();
+	v_ = player_camera_->get_v();
+
+	mvp_ = player_camera_->get_mvp();
+
+	// must can not is 0.0, so i write 0.001f
+	light_position_ = glm::vec3(3.001f, 4.0f, 5.0f);
+	glm::mat4 light_projection = glm::ortho<float>(-10, 10, -10, 10, -10, 20);
+	glm::mat4 light_view = glm::lookAt(light_position_, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 light_model = glm::mat4(1.0);
+
+	glm::mat4 bias_matrix = glm::mat4(
+		0.5, 0.0, 0.0, 0.0,
+		0.0, 0.5, 0.0, 0.0,
+		0.0, 0.0, 0.5, 0.0,
+		0.5, 0.5, 0.5, 1.0
+	);
+
+	light_mvp_ = light_projection * light_view * light_model;
+	bias_light_mvp_ = bias_matrix * light_mvp_;
+
+}
+
+void Shadow::SimpeVersionInit()
+{
+	// simple version
+	de::ShaderInfo shadow_shaders[] =
+	{
+		{ GL_VERTEX_SHADER, "shaders/shadow/shadowmap.vs.glsl" },
+		{ GL_FRAGMENT_SHADER, "shaders/shadow/shadowmap.fs.glsl" },
+		{ GL_NONE }
+	};
+
+	shadow_program_ = shader_->LoadShaders(shadow_shaders);
+	shadow_mvp_loc_ = gl_->glGetUniformLocation(shadow_program_, "mvp");
+	shadow_bias_mvp_loc_ = gl_->glGetUniformLocation(shadow_program_, "bias_mvp");
+	shadow_map_tex_loc_ = gl_->glGetUniformLocation(shadow_program_, "shadowMap");
+
+	// simpe version
+	gl_->glUseProgram(shadow_program_);
+	gl_->glUniform1i(shadow_map_tex_loc_, 0);
+}
+
+void Shadow::AdvanceVersionInit()
+{
+	// advance version
+	de::ShaderInfo advance_shadow_shaders[] =
+	{
+		{ GL_VERTEX_SHADER, "shaders/shadow/shadowmap_advance.vs.glsl" },
+		{ GL_FRAGMENT_SHADER, "shaders/shadow/shadowmap_advance.fs.glsl" },
+		{ GL_NONE }
+	};
+
+	advance_shadow_program_ = shader_->LoadShaders(advance_shadow_shaders);
+	advance_m_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "M");
+	advance_v_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "V");
+	advance_mvp_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "MVP");
+	advance_bias_matrix_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "DepthBiasMVP");
+	advance_shadow_tex_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "shadowMap");
+	advance_light_pos_loc_ = gl_->glGetUniformLocation(advance_shadow_program_, "LightInvDirection_worldspace");
+	
+	//advance version
+	gl_->glUseProgram(shadow_program_);
+	gl_->glUniform1i(advance_shadow_tex_loc_, 0);
+
+}
+
+void Shadow::SimpeVersionPaint()
+{
+	// simple version
+	gl_->glUseProgram(shadow_program_);
+	gl_->glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	gl_->glUniformMatrix4fv(shadow_mvp_loc_, 1, GL_FALSE, mvp_.constData());
+	gl_->glUniformMatrix4fv(shadow_bias_mvp_loc_, 1, GL_FALSE, &bias_light_mvp_[0][0]);
+}
+
+void Shadow::AdvanceVersionPaint()
+{
+	gl_->glUseProgram(advance_shadow_program_);
+	gl_->glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	gl_->glUniformMatrix4fv(advance_m_loc_, 1, GL_FALSE, m_.constData());
+	gl_->glUniformMatrix4fv(advance_v_loc_, 1, GL_FALSE, v_.constData());
+	gl_->glUniformMatrix4fv(advance_mvp_loc_, 1, GL_FALSE, mvp_.constData());
+	gl_->glUniformMatrix4fv(advance_bias_matrix_loc_, 1, GL_FALSE, &bias_light_mvp_[0][0]);
+	gl_->glUniform3fv(advance_light_pos_loc_, 1, &light_position_[0]);
+}
+
+void Shadow::SimpeVersionDisable()
+{
+	gl_->glDisable(GL_CULL_FACE);
+	gl_->glDisable(GL_DEPTH_TEST);
+}
+
+void Shadow::AdvanceVersionDisable()
+{
+	gl_->glDisable(GL_CULL_FACE);
+	gl_->glDisable(GL_DEPTH_TEST);
+}
+
+void Shadow::SimpeVersionClear()
+{
 	gl_->glDeleteProgram(depth_program_);
 	gl_->glDeleteProgram(shadow_program_);
+	gl_->glDeleteTextures(1, &depth_texture_);
+	gl_->glDeleteFramebuffers(1, &depth_fbo_);
+
+	model_->Free();
+}
+
+void Shadow::AdvanceVersionClear()
+{
+	gl_->glDeleteProgram(depth_program_);
+	gl_->glDeleteProgram(advance_shadow_program_);
 	gl_->glDeleteTextures(1, &depth_texture_);
 	gl_->glDeleteFramebuffers(1, &depth_fbo_);
 
--- shadow.h
+++ shadow.h
@@ -4,6 +4,8 @@
 #define SHADOW_H
 
 #include "dview.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 #include <qopenglfunctions_4_3_core.h>
 
@@ -18,17 +20,49 @@ public:
 	virtual void disableGL();
 	virtual void clearGL();
 
+	void InitData();
+
+	void SimpeVersionInit();
+	void AdvanceVersionInit();
+	void SimpeVersionPaint();
+	void AdvanceVersionPaint();
+	void SimpeVersionDisable();
+	void AdvanceVersionDisable();
+	void SimpeVersionClear();
+	void AdvanceVersionClear();
+
 private:
 	GLuint depth_program_;
 	GLuint shadow_program_;
+	GLuint advance_shadow_program_;
 
 	GLuint depth_mvp_loc_;
+
+	GLuint depth_texture_;
+	GLuint depth_fbo_;
+
+	// simple version
 	GLuint shadow_mvp_loc_;
 	GLuint shadow_bias_mvp_loc_;
 	GLuint shadow_map_tex_loc_;
 
-	GLuint depth_texture_;
-	GLuint depth_fbo_;
+	//advance version
+	GLuint advance_m_loc_;
+	GLuint advance_v_loc_;
+	GLuint advance_mvp_loc_;
+	GLuint advance_light_pos_loc_;
+	GLuint advance_bias_matrix_loc_;
+	GLuint advance_shadow_tex_loc_;
+
+	bool kAdvance_;
+	QMatrix4x4 m_;
+	QMatrix4x4 v_;
+	QMatrix4x4 mvp_;
+
+	glm::vec3 light_position_;
+	glm::mat4 light_mvp_;
+	glm::mat4 bias_light_mvp_;
+
 
 };
 
