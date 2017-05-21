3b255f9ea4eca8d20ec37f8ad47b4d9dded168de
Eric <nameericchen@gmail.com>
2017-05-21 11:56:08 +0800

    class diagram update

--- media/shaders/specular_material/specular_material.fs.glsl
+++ media/shaders/specular_material/specular_material.fs.glsl
@@ -1,20 +1,15 @@
 #version 430 core
 
-uniform vec3 light_pos;
-
 in vec4 out_vposition;
 in vec4 out_normal;
+in vec4 out_light_pos;
 
 out vec4 color;
 
 void main(){
 	vec4 ambient = vec4(0.5f, 0.0f, 0.0f, 1.0f);
 
-	vec4 dir = vec4(light_pos,1.0f) - out_vposition;
-	float light_len = length(dir);
-	
-	dir = dir / light_len;
-
+	vec4 dir =  out_light_pos - out_vposition;
 	float angle = max(0.0f, dot(normalize(dir),normalize(out_normal)));
 
 	color = ambient + vec4(angle);
--- media/shaders/specular_material/specular_material.vs.glsl
+++ media/shaders/specular_material/specular_material.vs.glsl
@@ -6,13 +6,16 @@ layout(location = 2) in vec3 vNormal;
 uniform mat4 m;
 uniform mat4 v;
 uniform mat4 p;
+uniform vec3 light_pos;
 
 out vec4 out_vposition;
 out vec4 out_normal;
+out vec4 out_light_pos;
 
 void main(){
-	out_vposition = vec4(vPosition, 1.0f);
-	out_normal = vec4(vNormal, 1.0f);
+	out_vposition = v * m * vec4(vPosition, 1.0f);
+	out_normal = v * m * vec4(vNormal, 1.0f);
+	out_light_pos = v *m * vec4(light_pos, 1.0f);
 
 	gl_Position = p * v * m * vec4(vPosition, 1.0f);
 }
